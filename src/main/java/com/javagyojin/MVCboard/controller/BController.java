da0a392bcafc95f2044900ec6e81e3c5bd5f4682
ici806 <evergreen0208@hanmail.net>
2022-04-27 15:02:47 +0900

    mvc_jdbc

--- src/main/java/com/javagyojin/MVCboard/controller/BController.java
+++ src/main/java/com/javagyojin/MVCboard/controller/BController.java
@@ -2,11 +2,14 @@ package com.javagyojin.MVCboard.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javagyojin.MVCboard.command.*;
+import com.javagyojin.MVCboard.util.Constant;
 
 
 @Controller
@@ -14,6 +17,14 @@ public class BController {
 	
 	BCommand command = null;
 	
+	private JdbcTemplate template;
+	
+	@Autowired
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+		Constant.template = this.template;
+	}
+
 	@RequestMapping(value = "/list")
 	public String list(Model model) {
 		
